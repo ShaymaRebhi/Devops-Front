be6d0a835feafbfa13d30d2875a6ea4ec7359a26
OumaymaFarhani <oumaymafarhani@esprit.tn>
2022-11-07 13:18:08 +0100

    environnement.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_url: 'http://172.10.0.140/SpringMVC/'
 };
 
 /*
